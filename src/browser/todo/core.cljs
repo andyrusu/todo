7475d5e1cb23dc9ebcb2fd85cb39022e5ad9f543
Andy Rusu <rusu.andy1@gmail.com>
2020-02-02 12:16:06 +0200

    added reagent

--- src/browser/todo/core.cljs
+++ src/browser/todo/core.cljs
@@ -1,1 +1,15 @@
-(ns todo.core)
+(ns todo.core
+  (:require [reagent.core :as r]))
+
+(def click-count (r/atom 0))
+
+(defn counting-component []
+  [:div
+   "The atom " [:code "click-count"] " has value: "
+   @click-count ". "
+   [:input {:type "button" :value "Click me!"
+            :on-click #(swap! click-count inc)}]])
+
+(defn ^:export run []
+  (r/render [counting-component]
+            (js/document.getElementById "app")))
