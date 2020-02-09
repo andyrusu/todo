6ceaf28fe2bc4307c6f14c448d6ef4d12871026b
Andy Rusu <rusu.andy1@gmail.com>
2020-02-09 20:14:22 +0200

    Added electron

--- src/electron/todo/main.cljs
+++ src/electron/todo/main.cljs
@@ -1,1 +1,14 @@
-(ns todo.main)
+(ns todo.main
+  (:require [electron :as el]))
+
+(def app (.-app el))
+;(def browser-window (.-BrowserWindow el))
+
+(defn create-window []
+  (let [window (new (.-BrowserWindow el) #js {:width 800
+                                              :height 600
+                                              :webPreferences {:nodeIntegration true}})]
+    (.loadFile window "public/index.html")))
+
+(defn ^:export main []
+  (.then (.whenReady app) create-window))
